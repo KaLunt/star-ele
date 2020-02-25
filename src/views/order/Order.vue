a498e23fcc8f07dfa11f053e75eca196949b6fa5
Kalunt <249608433@qq.com>
2020-02-25 22:29:59 +0800

    项目初始化，引入vant-ui库

---
+++ src/views/order/Order.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<h2>Order</h2>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"Order"
+	}
+</script>
+
+<style>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  resolve:{
+    extensions: [],
+    alias: {
+      'components':'@components',
+      'common':'components/common',
+      'content':'components/common',
+      'assets':'@/assets',
+      'network':'@/newwork',
+      'views':'@views',
+    }
+  }
+}
